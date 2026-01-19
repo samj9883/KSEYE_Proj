9808fce69e4375d8c531936fd76057ba3f341c7c
Sam Jones <samj9883@gmail.com>
2026-01-19 09:56:30 +0000

    refactor: using get instead of just print for the ticket components

--- src/triage.py
+++ src/triage.py
@@ -3,9 +3,16 @@ import json
 with open("data/tickets.json", "r", encoding="utf-8") as file:
     tickets = json.load(file)
 
+
+
 for ticket in tickets:
-    print(f"ID: {ticket['id']}")
-    print(f"Created At: {ticket['created_at']}")
-    print(f"Channel: {ticket['channel']}")
-    print(f"Message: {ticket['message']}")
+    ticket_id = ticket.get("id", "UNKNOWN")
+    created_at = ticket.get("created_at","UNKNOWN")
+    channel = ticket.get("channel", "UNKNOWN")
+    message = ticket.get("message", "")
+
+    print(f"[{ticket_id}], Created At: {created_at} ({channel}) {message}")
+
     print("-" * 40)
+
+
